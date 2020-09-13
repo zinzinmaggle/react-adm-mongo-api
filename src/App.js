3a4f53c021fd8ee5c8768bf0c6188d6ce45820ae
grossac <frederic-quemper@outlook.com>
2020-09-13 20:34:04 +0200

    fix url api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
-const dataProvider = jsonServerProvider('https://react-adm-mongo-api.vercel.app/');
+const dataProvider = jsonServerProvider('https://react-adm-mongo-api.vercel.app/api/');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
