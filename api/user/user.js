e2a5d467da9db1b6892e99743a037a14ddd46161
grossac <frederic-quemper@outlook.com>
2020-09-15 14:55:44 +0200

    test

---
+++ api/user/user.js
@@ -0,0 +1,60 @@
+// Import Dependencies
+const url = require('url')
+const MongoClient = require('mongodb').MongoClient
+
+// Create cached connection variable
+let cachedDb = null
+
+// A function for connecting to MongoDB,
+// taking a single parameter of the connection string
+async function connectToDatabase(uri) {
+  // If the database connection is cached,
+  // use it instead of creating a new connection
+  if (cachedDb) {
+    return cachedDb
+  }
+
+  // If no connection is cached, create a new one
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true })
+
+  // Select the database through the connection,
+  // using the database path of the connection string
+  const db = await client.db(url.parse(uri).pathname.substr(1))
+
+  // Cache the database connection and return the connection
+  cachedDb = db
+  return db
+}
+
+// The main, exported, function of the endpoint,
+// dealing with the request and subsequent response
+module.exports = async (req, res) => {
+
+  // Get a database connection, cached or otherwise,
+  // using the connection string environment variable as the argument
+  const db = await connectToDatabase(process.env.MONGODB_URI)
+
+  // Select the "users" collection from the database
+  const collection = await db.collection('user')
+
+  const { end, order,sort,start } = req.query
+
+  let users = await collection.find({}).toArray();
+  
+  switch(req.method){
+    case 'GET':
+        result = await collection.findOne({id : req.query.id});
+      break;
+    case 'PUT':
+      result =  await collection.update({id : req.query.id},req.body);
+      break;
+    case 'DELETE':
+      result = await collection.insert(req.body)
+      break;
+  }
+
+  // Select the users collection from the database
+  res.setHeader("x-total-count", users.length);
+  // Respond with a JSON string of all users in the collection
+  res.redirect('/user')
+}
